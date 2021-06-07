7db3622d52ffd7ca3bfa8302e93fe207bd4a87f9
tylermatrix <tylermaatrix@gmail.com>
2021-06-06 18:43:39 -0700

    added routing and setup pages

--- src/App.js
+++ src/App.js
@@ -3,11 +3,44 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 import styled from 'styled-components';
 
+import {Home, Products, SingleProduct, About, Cart, Error, Checkout, Private} from './pages'
+
 
 function App() {
-  return <div>
-    <h4>Comfy Sloth</h4>
-  </div>
+  return <Router>
+    <Navbar/>
+    <Sidebar/>
+    <Switch>
+      <Route exact path ="/">
+        <Home></Home>
+      </Route>
+      
+      <Route exact path ="/about">
+        <About></About>
+      </Route>
+      
+      <Route exact path ="/cart">
+        <Cart></Cart>
+      </Route>
+      
+      <Route exact path ="/products">
+        <Products></Products>
+      </Route>
+      
+      <Route exact path="/products/:id" children={ <SingleProduct></SingleProduct>}>
+
+      </Route>
+
+      <Route exact path="/checkout">
+        <Checkout></Checkout>
+      </Route>
+      
+      <Route  path ="/*">
+        <Error></Error>
+      </Route>
+    </Switch>
+    <Footer/>
+  </Router>
 }
 
 export default App
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Home from './HomePage';
+import Products from './ProductsPage';
+import SingleProduct from './SingleProductPage';
+import Cart from './CartPage';
+import About from './AboutPage';
+import Error from './ErrorPage';
+import Checkout from './CheckoutPage';
+import Private from './PrivateRoute';
+
+
+export { Home, Cart, Products, SingleProduct, About, Error, Checkout, Private } 
