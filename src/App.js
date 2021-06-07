3833249be8d0659b0e9536dffedc083cb853ccf6
tylermatrix <tylermaatrix@gmail.com>
2021-06-06 18:17:44 -0700

    added skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+import styled from 'styled-components';
+
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return <div>
+    <h4>Comfy Sloth</h4>
+  </div>
 }
 
 export default App
