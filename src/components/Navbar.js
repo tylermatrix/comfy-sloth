fd219818ba94527a6366c69326ea2f049f72f33a
tylermatrix <tylermaatrix@gmail.com>
2021-06-07 20:44:49 -0700

    creating navbar structure

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,24 @@ import { useProductsContext } from '../context/products_context'
 import { useUserContext } from '../context/user_context'
 
 const Nav = () => {
-  return <h4>navbar</h4>
+  return <NavContainer>
+    <div className="nav-center">
+      <div className="nav-header">
+        <Link to="/">
+          <img src={logo} alt="comfy sloth" />
+        </Link>
+        <button type="button" className="nav-toggle">
+          <FaBars/>
+        </button>
+      </div>
+      <ul className="nav-links">
+        {links.map((link) => {
+          const {id, text,url} = link;
+          <li key={id}>{text}</li>
+        })}
+      </ul>
+    </div>
+  </NavContainer>
 }
 
 const NavContainer = styled.nav`
