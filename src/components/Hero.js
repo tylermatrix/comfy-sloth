aa362bdbe6d82d0c24809505fc0af226303d3980
tylermatrix <tylermaatrix@gmail.com>
2021-06-12 15:29:50 -0700

    creating base for Home Page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,18 @@ import heroBcg from '../assets/hero-bcg.jpeg'
 import heroBcg2 from '../assets/hero-bcg-2.jpeg'
 
 const Hero = () => {
-  return <h4>hero</h4>
+  return <Wrapper className="section-center">
+    <article className="content">
+      <h1>Design you <br />
+      comfort zone</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ipsa expedita ullam quibusdam eveniet, praesentium nihil commodi laboriosam dolores inventore?</p>
+      <Link to="/" className="btn hero-btn">Shop Now</Link>
+    </article>
+    <article className="container">
+      <img src={heroBcg} alt="nice table" className="main-img" />
+      <img src={heroBcg2} alt="person working" className="accent-img" />
+    </article>
+  </Wrapper>
 }
 
 const Wrapper = styled.section`
