9153a07c168cfbf3dd66120c7ff7698ed91b357a
tylermatrix <tylermaatrix@gmail.com>
2021-06-21 16:38:27 -0700

    implemented star rating system

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,9 +1,62 @@
-import React from 'react'
-import styled from 'styled-components'
-import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
-const Stars = () => {
-  return <h4>stars</h4>
-}
+import React from "react";
+import styled from "styled-components";
+import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
+const Stars = ({ stars, reviews }) => {
+  return (
+    <Wrapper>
+      <div className="stars">
+        {/* Start of stars */}
+        <span>
+          {stars >= 1 ? (
+            <BsStarFill></BsStarFill>
+          ) : stars >= 0.5 ? (
+            <BsStarHalf></BsStarHalf>
+          ) : (
+            <BsStar />
+          )}
+        </span>
+        <span>
+          {stars >= 2 ? (
+            <BsStarFill></BsStarFill>
+          ) : stars >= 1.5 ? (
+            <BsStarHalf></BsStarHalf>
+          ) : (
+            <BsStar />
+          )}
+        </span>
+        <span>
+          {stars >= 3 ? (
+            <BsStarFill></BsStarFill>
+          ) : stars >= 2.5 ? (
+            <BsStarHalf></BsStarHalf>
+          ) : (
+            <BsStar />
+          )}
+        </span>
+        <span>
+          {stars >= 4 ? (
+            <BsStarFill></BsStarFill>
+          ) : stars >= 3.5 ? (
+            <BsStarHalf></BsStarHalf>
+          ) : (
+            <BsStar />
+          )}
+        </span>
+        <span>
+          {stars === 5 ? (
+            <BsStarFill></BsStarFill>
+          ) : stars >= 4.5 ? (
+            <BsStarHalf></BsStarHalf>
+          ) : (
+            <BsStar />
+          )}
+        </span>
+      </div>
+      {/* End of stars */}
+      <p className="reviews">({reviews} customer reviews)</p>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div`
   display: flex;
@@ -18,5 +71,5 @@ const Wrapper = styled.div`
     margin-bottom: 0;
   }
   margin-bottom: 0.5rem;
-`
-export default Stars
+`;
+export default Stars;
