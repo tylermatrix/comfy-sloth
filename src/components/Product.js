e297f1250b8577c254815724d793e2f3b2be5fbc
tylermatrix <tylermaatrix@gmail.com>
2021-06-21 15:55:37 -0700

    added images and active image class conditional to the single product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import styled from 'styled-components'
-import { formatPrice } from '../utils/helpers'
-import { FaSearch } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import React from "react";
+import styled from "styled-components";
+import { formatPrice } from "../utils/helpers";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Product = ({ image, name, price, id }) => {
-  return <Wrapper>
-    <div className="container">
-      <img src={image} alt={name} />
-      <Link to={`/products/${id}`} className="link">
-        <FaSearch></FaSearch>
-      </Link>
-    </div>
-    <footer>
-      <h5>{name}</h5>
-      <p>${formatPrice(price)}</p>
-    </footer>
-  </Wrapper>
-}
+  return (
+    <Wrapper>
+      <div className="container">
+        <img src={image} alt={name} />
+        <Link to={`/products/${id}`} className="link">
+          <FaSearch></FaSearch>
+        </Link>
+      </div>
+      <footer>
+        <h5>{name}</h5>
+        <p>${formatPrice(price)}</p>
+      </footer>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.article`
   .container {
@@ -74,5 +76,5 @@ const Wrapper = styled.article`
       color: var(--clr-primary-5);
     letter-spacing: var(--spacing);
   }
-`
-export default Product
+`;
+export default Product;
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -7,33 +7,49 @@ import {
   GET_SINGLE_PRODUCT_BEGIN,
   GET_SINGLE_PRODUCT_SUCCESS,
   GET_SINGLE_PRODUCT_ERROR,
-} from '../actions'
+} from "../actions";
 
 const products_reducer = (state, action) => {
   if (action.type === SIDEBAR_OPEN) {
-    return { ...state, isSidebarOpen: true }
+    return { ...state, isSidebarOpen: true };
   }
 
   if (action.type === SIDEBAR_CLOSE) {
-    return { ...state, isSidebarOpen: false }
+    return { ...state, isSidebarOpen: false };
   }
 
   if (action.type === GET_PRODUCTS_BEGIN) {
-    return { ...state, products_loading: true }
+    return { ...state, products_loading: true };
   }
 
   if (action.type === GET_PRODUCTS_SUCCESS) {
     const featured_products = action.payload.filter((product) => {
-      product_feature === true
-    })
-    return { ...state, products_loading: false, products: action.payload, featured_products }
+      product_feature === true;
+    });
+    return {
+      ...state,
+      products_loading: false,
+      products: action.payload,
+      featured_products,
+    };
   }
 
   if (action.type === GET_PRODUCTS_ERROR) {
-    return { ...state, products_loading: false, products_error: true }
+    return { ...state, products_loading: false, products_error: true };
   }
 
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+  if (action.type === GET_SINGLE_PRODUCT_BEGIN) {
+    return {
+      ...state,
+      single_product_loading: true,
+      single_product_error: false,
+    };
+  }
 
-export default products_reducer
+  if (action.type === GET_SINGLE_PRODUCT_SUCCESS) {
+  }
+
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default products_reducer;
