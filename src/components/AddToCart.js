bce3ab34a508177bc46ac2aacf684364b33cdc59
tylermatrix <tylermaatrix@gmail.com>
2021-06-21 16:55:04 -0700

    implemented buttons to add or remove itemswhen adding to cart

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -11,6 +11,26 @@ const AddToCart = ({ product }) => {
 
   const [mainColor, setMainColor] = useState(colors[0]);
 
+  const [amount, setAmount] = useState(1);
+
+  const increase = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1;
+      if (tempAmount > stock) {
+        tempAmount = stock;
+      }
+    });
+    return tempAmount;
+  };
+  const decrease = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1;
+      if (tempAmount < 1) {
+        tempAmount = 1;
+      }
+    });
+    return tempAmount;
+  };
   return (
     <Wrapper>
       <div className="colors">
@@ -35,7 +55,16 @@ const AddToCart = ({ product }) => {
         </div>
       </div>
 
-      <div className="btn-container"></div>
+      <div className="btn-container">
+        <AmountButtons
+          amount={amount}
+          increase={increase}
+          decrease={decrease}
+        ></AmountButtons>
+        <Link to="/cart" className="btn">
+          Add to cart
+        </Link>
+      </div>
     </Wrapper>
   );
 };
